f52acf3dd88fc89fa2cf01f265ffa6f3f3228aa3
Christopher N. Hesse <raymanfx@gmail.com>
2015-11-02 21:58:45 +0100

    treltexx: CM-ify
    Change-Id: I80bca97b0f447d521b3d30a89d45cd316b33d314

--- cm.mk
+++ cm.mk
@@ -2,16 +2,16 @@
 PRODUCT_RELEASE_NAME := treltexx
 
 # Inherit some common EOS stuff.
-$(call inherit-product, vendor/eos/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit nfc enhanced configuration
-#$(call inherit-product, vendor/eos/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/treltexx/device.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := eos_treltexx
+PRODUCT_NAME := cm_treltexx
 PRODUCT_DEVICE := treltexx
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-N910C
