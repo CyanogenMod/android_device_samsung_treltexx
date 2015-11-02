d65c8c54206eb20ab0ab362ae86b55c381fe9c27
Christopher N. Hesse <raymanfx@gmail.com>
2015-11-02 21:55:33 +0100

    treltexx: Switch to the new Exynos 5433 hardware repo
    Change-Id: I49811f22af713ce28f0f6aad043d47fa66846a32

--- device.mk
+++ device.mk
@@ -379,7 +379,7 @@ $(call inherit-product-if-exists, build/target/product/full.mk)
 $(call inherit-product-if-exists, vendor/samsung/treltexx/treltexx-vendor.mk)
 
 # call Samsung LSI board support package
-$(call inherit-product, hardware/samsung_slsi/exynos5433/exynos5433.mk)
+$(call inherit-product, hardware/samsung_slsi-cm/exynos5433/exynos5433.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_treltexx
