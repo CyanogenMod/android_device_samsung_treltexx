96de55ac203ef4a5d22c5bc06baa215a2b2f092b
Christopher N. Hesse <raymanfx@gmail.com>
2015-12-15 17:28:07 +0100

    treltexx: Kang GPS interposition library from manta
    Change-Id: I04c120717f3bc105b61e8d11bfed4e3e461e9823

--- device-common.mk
+++ device-common.mk
@@ -211,6 +211,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/configs/gps/gps.xml:system/etc/gps.xml
 
+PRODUCT_PACKAGES += \
+    libdmitry
+
 ###########################################################
 ### CAMERA
 ###########################################################
