2b6959de3e2b99139f5af228eae7f8c61164f912
Christopher N. Hesse <raymanfx@gmail.com>
2015-12-26 23:02:13 +0100

    treltexx: Replace hwrevision daemon with bash script
    Change-Id: Ia0931625e1353d53cbb0b90ce28bf4d522dd2a86

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -69,6 +69,13 @@ LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.modem.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.modem.sh
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.sec.boot.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
