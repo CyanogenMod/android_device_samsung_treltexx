62f92f2920bd2a61564a1ffbb088c3b08254eacf
Christopher N. Hesse <raymanfx@gmail.com>
2016-02-01 13:50:22 +0100

    Revert "treltexx: Workaround invalid gralloc handles"
    We now have a better workaround for this in hardware/samsung_slsi.
    
    This reverts commit eeb7b40640d6e167eb83bb75475d10583b8c4f97.
    
    Change-Id: I7760e765eb0cd1c54312aa264bbd664bc63e37de

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -67,7 +67,6 @@ BOARD_OVERRIDE_RS_CPU_VARIANT_32 := cortex-a53
 # misses the time to swap buffers (in cases where it takes 16ms or
 # less). Use 3 to avoid timing issues.
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
-BOARD_EGL_NEEDS_HANDLE_VALUE := true
 
 # Exynos display
 BOARD_USES_VIRTUAL_DISPLAY := true
