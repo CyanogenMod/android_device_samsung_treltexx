83000b47a78000e8c6154f62e61bdd6ce7cf7bff
Christopher N. Hesse <raymanfx@gmail.com>
2016-02-08 13:45:31 +0100

    treltexx: Reorganize ramdisk dir
    Change-Id: Ib282036372e0c190a55148c71264e0d4afa2f9fa

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -162,7 +162,7 @@ TARGET_IGNORE_RO_BOOT_REVISION := true
 BOARD_RIL_CLASS := ../../../$(LOCAL_PATH)/ril
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.universal5433
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/ramdisk/fstab.universal5433
 
 # SELinux
 BOARD_SEPOLICY_DIRS := \
