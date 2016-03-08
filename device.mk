31847fa51c5ec6bdbbffe0924844256abc215728
Christopher N. Hesse <raymanfx@gmail.com>
2016-03-08 23:11:48 +0100

    treltexx: Import audio configs
    Change-Id: I54385be881bc575f5b9a66f357ee1e71e2778e87

--- device.mk
+++ device.mk
@@ -22,5 +22,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ril/sbin/cbd:root/sbin/cbd
 
+###########################################################
+### AUDIO
+###########################################################
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+
 # call the proprietary setup
 $(call inherit-product-if-exists, vendor/samsung/treltexx/treltexx-vendor.mk)
