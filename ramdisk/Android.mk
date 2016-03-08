c767051024fe08280acb5f87b28137199325fff8
Christopher N. Hesse <raymanfx@gmail.com>
2016-03-08 13:47:35 +0100

    trhpltexx: rootdir -> ramdisk
    Change-Id: Ica4b14a60dac8b6537f12e3933741c84b8173a47

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -6,7 +6,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.modem.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= BOOT
-LOCAL_SRC_FILES		:= etc/init.modem.rc
+LOCAL_SRC_FILES		:= init.modem.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
