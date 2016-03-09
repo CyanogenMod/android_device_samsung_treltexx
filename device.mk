bc623f9610c2bc65d1326efbf5e216e32a38e755
Christopher N. Hesse <raymanfx@gmail.com>
2016-03-09 10:10:50 +0100

    treltexx: Import BT vendor config
    Change-Id: Id7604c794b976024dd2f3e854bc6914d5fde7b15

--- device.mk
+++ device.mk
@@ -29,5 +29,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+###########################################################
+### BLUETOOTH
+###########################################################
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
+
 # call the proprietary setup
 $(call inherit-product-if-exists, vendor/samsung/treltexx/treltexx-vendor.mk)
