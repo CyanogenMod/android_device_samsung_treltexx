c80d1684006c9800b7f7e76b6d08dafe3229fbc1
travis mills <travismills82@gmail.com>
2016-08-01 19:11:28 +0800

    treltexx: Housekeeping
    Change-Id: I871f6a34fd3692a333a2dfbda189a9b6d1ad4481

--- device.mk
+++ device.mk
@@ -1,30 +1,18 @@
 LOCAL_PATH := device/samsung/treltexx
 
-###########################################################
-### FLAT DEVICE TREE
-###########################################################
-
+# FLAT DEVICE TREE
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/dtb.img:dt.img
 
-###########################################################
-### RAMDISK
-###########################################################
-
+# RAMDISK
 PRODUCT_PACKAGES += \
     init.modem.rc
 
-###########################################################
-### AUDIO
-###########################################################
-
+# AUDIO
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
-###########################################################
-### BLUETOOTH
-###########################################################
-
+# BLUETOOTH
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
 
